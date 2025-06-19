10962d18f52f093ed9a896359371a43ccf97cd07
Varad Jagtap <varadjagtap31@gmail.com>
2025-06-19 17:55:23 +0530

    update

--- admin/components/sw_save_vendor.php
+++ admin/components/sw_save_vendor.php
@@ -6,7 +6,7 @@ if($_FILES['passbook']['size']>0)
     $passbook=time().$_FILES['passbook']['name'];
     move_uploaded_file($_FILES['passbook']['tmp_name'], "uploads/".$passbook);
 }
-$query="INSERT INTO vendors(vendor_name,type,address,gst_pan,phone_no,email,bank_name,acc_no,ifsc,bank_address,city,district,state,pincode,status,passbook) VALUES ('$vendor_name','$type','$address','$gst_pan','$phone_no','$email','$bank_name','$acc_no','$ifsc','$bank_address','$city','$district','$state','$pincode','$status','$passbook')";
+$query="INSERT INTO vendors(vendor_name,vendor_type_id,address,gst_pan,phone_no,email,bank_name,acc_no,ifsc,bank_address,city,district,state,pincode,status,passbook) VALUES ('$vendor_name','$vendor_type_id','$address','$gst_pan','$phone_no','$email','$bank_name','$acc_no','$ifsc','$bank_address','$city','$district','$state','$pincode','$status','$passbook')";
 $result=mysqli_query($conn,$query);
 header("Location: ../master/sw_manage_vendor.php");
 ?>
--- admin/master/edit_sw_vendor.php
+++ admin/master/edit_sw_vendor.php
@@ -1,50 +1,38 @@
 <?php
 include "navbar.php";
+extract($_GET);
+$query="SELECT * FROM vendors WHERE vendor_id='$vendor_id'";
+$result=mysqli_query($conn, $query);
+$vendor_row=mysqli_fetch_assoc($result);
 ?>
-<div class="row g-3">
+<div class="row">
     <div class="col-md-12">
-        <button class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#modalCenter">Add
-            Vendor</button>
-    </div>
-</div>
-<div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalCenterTitle">Manage Vendor</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
+        <div class="card card-body">
+            <h5>Manage Vendor</h5>
+            <div class="row g-3">
                 <form action="../components/sw_save_vendor.php" method="POST" enctype="multipart/form-data">
                     <div class="row g-3">
                         <div class="col-md-12">
                             <div class="form-group">
+                                <input type="hidden" name="vendor_id" value="<?=$vendor_row['vendor_id']; ?>">
                                 <label for="vendor_name">Vendor Name</label>
                                 <input type="text" class="form-control" id="vendor_name" name="vendor_name"
-                                    placeholder="" required>
+                                    placeholder="" value="<?=$vendor_row['vendor_name']?>" required>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="address">Vendor Address</label>
-                                <textarea class="form-control" name="address" id=""></textarea>
+                                <textarea class="form-control" name="address" id=""><?=$vendor_row['address']?></textarea>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="type">Vendor Type</label>
-                                <select class="form-select" id="type" name="vendor_type_id" required>
+                                <select class="form-select" id="type" name="type" required>
                                     <option value="">Select Type</option>
-                                    <?php
-                                    $query="SELECT * FROM vendors_type";
-                                    $result=mysqli_query($conn, $query);
-                                    while($vendor_type_row=mysqli_fetch_assoc($result))
-                                    {
-                                       ?>
-                                       <option value="<?=$vendor_type_row['vendor_type_id']?>"><?=$vendor_type_row['vendor_type']?></option>
-                                       <?php
-                                    }
-                                    ?>
+                                    <option value="Production" <?=($vendor_row['type']=='Production') ? 'selected' : ''?>>Production</option>
+                                    <option value="Publication" <?=($vendor_row['type']=='Publication') ? 'selected' : ''?>>Publication</option>
                                 </select>
                             </div>
                         </div>
@@ -52,48 +40,48 @@ include "navbar.php";
                             <div class="form-group">
                                 <label for="gst_pan">GST/PAN No</label>
                                 <input type="text" class="form-control" id="gst_pan" name="gst_pan" placeholder=""
-                                    required>
+                                 value="<?=$vendor_row['gst_pan']?>"   required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="phone_no">Contact No</label>
                                 <input type="number" class="form-control" id="phone_no" name="phone_no" placeholder=""
-                                    required>
+                                  value="<?=$vendor_row['phone_no']?>"  required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="email">Vendor Email</label>
                                 <input type="email" class="form-control" id="email" name="email" placeholder=""
-                                    required>
+                                 value="<?=$vendor_row['email']?>"   required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="city">City</label>
-                                <input type="text" class="form-control" id="city" name="city" placeholder="" required>
+                                <input type="text" class="form-control" id="city" name="city" placeholder="" value="<?=$vendor_row['city']?>" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="district">District</label>
                                 <input type="text" class="form-control" id="district" name="district" placeholder=""
-                                    required>
+                                 value="<?=$vendor_row['district']?>"   required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="state">State</label>
                                 <input type="state" class="form-control" id="state" name="state" placeholder=""
-                                    required>
+                                 value="<?=$vendor_row['state']?>"   required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="pincode">Pincode</label>
                                 <input type="number" class="form-control" id="pincode" name="pincode" placeholder=""
-                                    required>
+                                 value="<?=$vendor_row['pincode']?>"   required>
                             </div>
                         </div>
                         <h5>Bank Details</h5>
@@ -101,14 +89,14 @@ include "navbar.php";
                             <div class="form-group">
                                 <label for="bank_name">Bank Name</label>
                                 <input type="text" class="form-control" id="bank_name" name="bank_name" placeholder=""
-                                    required>
+                                 value="<?=$vendor_row['bank_name']?>"   required>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="bank_address">Bank Address</label>
                                 <textarea class="form-control" name="bank_address" id="bank_address"
-                                    oninput="validateMarathiAddress(this)"></textarea>
+                                    oninput="validateMarathiAddress(this)"><?=$vendor_row['bank_address']?></textarea>
                                 <small id="addressWarning" class="text-danger d-none">Commas (,) not allowed.</small>
                             </div>
                         </div>
@@ -116,13 +104,13 @@ include "navbar.php";
                             <div class="form-group">
                                 <label for="acc_no">Bank Account No</label>
                                 <input type="number" class="form-control" id="acc_no" name="acc_no" placeholder=""
-                                    required>
+                                value="<?=$vendor_row['acc_no']?>"    required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="ifsc">IFSC code</label>
-                                <input type="text" class="form-control" id="ifsc" name="ifsc" placeholder="" required>
+                                <input type="text" class="form-control" id="ifsc" name="ifsc" placeholder="" value="<?=$vendor_row['ifsc']?>" required>
                             </div>
                         </div>
                         <div class="col-md-6">
@@ -130,8 +118,8 @@ include "navbar.php";
                                 <label for="status">Status</label>
                                 <select class="form-select" id="status" name="status" required>
                                     <option value="">Select Status</option>
-                                    <option value="active">Active</option>
-                                    <option value="inactive">Inactive</option>
+                                    <option value="active" <?=($vendor_row['status']=='active') ? 'selected' : ''?>>Active</option>
+                                    <option value="inactive" <?=($vendor_row['status']=='inactive') ? 'selected' : ''?>>Inactive</option>
                                 </select>
                             </div>
                         </div>
@@ -142,6 +130,9 @@ include "navbar.php";
                                     required>
                             </div>
                         </div>
+                        <div class="col-md-12 text-center">
+                            <img class="w-50 h-100" src="../components/uploads/<?=$vendor_row['passbook']?>" alt="">
+                        </div>
                     </div>
                     <button class="btn btn-primary mt-3 float-end">Add Vendor</button>
                 </form>
@@ -149,67 +140,6 @@ include "navbar.php";
         </div>
     </div>
 </div>
-<div class="row mt-4">
-    <div class="col-md-12">
-        <div class="card card-body table-responsive">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-                <h5 class="mb-0">Vendor List</h5>
-                <div class="d-flex gap-2">
-                    <a href="sw_manage_vendor.php" class="btn btn-secondary btn-sm d-flex align-items-center">
-                        <i class="fa-solid fa-rotate-right"></i>
-                    </a>
-                    <form action="sw_manage_vendor.php" method="GET" class="d-flex">
-                        <div class="input-group input-group-sm">
-                            <input type="text" class="form-control" placeholder="Search" name="search">
-                            <button class="btn btn-primary" type="submit">
-                                <i class="fa-solid fa-magnifying-glass"></i>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <table class="table table-bordered table-hover text-center">
-                <thead>
-                    <tr>
-                        <th>srno</th>
-                        <th>Vendor Name</th>
-                        <th>Contact No</th>
-                        <th>Vendor Type</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $query = "SELECT * FROM vendors, vendors_type WHERE vendors.vendor_type_id = vendors_type.vendor_type_id";
-                    $result= mysqli_query($conn,$query);
-                    foreach($result as $key=>$vendor_row)
-                    {
-                        ?>
-                        <tr>
-                            <td><?=$key+1?></td>
-                            <td><?=$vendor_row['vendor_name']?></td>
-                            <td><?=$vendor_row['phone_no']?></td>
-                            <td><?=$vendor_row['vendor_type']?></td>
-                            
-                        </tr>
-                        <?php
-                    }
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
 <?php
 include "footer.php";
 ?>
-<script>
-function validateMarathiAddress(textarea) {
-    const warning = document.getElementById('addressWarning');
-    if (textarea.value.includes(',')) {
-        warning.classList.remove('d-none');
-    } else {
-        warning.classList.add('d-none');
-    }
-}
-</script>
